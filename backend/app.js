77f01407a2b42baaf15333a89aa605ff4e4c64f7
harshv9988 <harshv9988@gmail.com>
2021-02-23 20:28:21 +0530

    added error handling class and middleware

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const app = express();
+const errorMiddleware = require("./middlewares/errors");
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 //import all routes
 const products = require("./routes/product");
 app.use("/api/v1", products);
+
+//error middleware
+app.use(errorMiddleware);
 
 module.exports = app;
