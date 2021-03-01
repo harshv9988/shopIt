7864887a513290f74cf51fc50ff6f0684bda7947
harshv9988 <harshv9988@gmail.com>
2021-03-01 12:17:57 +0530

    added mongoose and cast error detection

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -47,10 +47,7 @@ exports.updateProduct = catchAsyncErrors(async (req, res, next) => {
     let product = await Product.findById(req.params.id);
 
     if (!product) {
-      return res.status(404).json({
-        success: false,
-        message: "Product not found",
-      });
+      return next(new ErrorHandler("Product not found", 404));
     }
 
     product = await Product.findByIdAndUpdate(req.params.id, req.body, {
@@ -77,10 +74,7 @@ exports.deleteProduct = catchAsyncErrors(async (req, res, next) => {
   const product = await Product.findById(req.params.id);
 
   if (!product) {
-    return res.status(404).json({
-      success: false,
-      message: "Product not found",
-    });
+    return next(new ErrorHandler("Product not found", 404));
   }
 
   await product.remove();
