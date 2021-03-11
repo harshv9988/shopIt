a39b33ef13bec020f70addc42a43c4386b0a67ba
harshv9988 <harshv9988@gmail.com>
2021-03-11 17:41:17 +0530

    completed order related routes

--- backend/routes/order.js
+++ backend/routes/order.js
@@ -1,10 +1,30 @@
 const express = require("express");
 const router = express.Router();
 
-const { newOrder } = require("../controllers/orderController");
+const {
+  newOrder,
+  getSingleOrder,
+  myOrders,
+  allOrders,
+  updateOrder,
+  deleteOrder,
+} = require("../controllers/orderController");
 
 const { isAuthenticatedUser, authorizeRoles } = require("../middlewares/auth");
 
 router.route("/order/new").post(isAuthenticatedUser, newOrder);
+router.route("/order/me").get(isAuthenticatedUser, myOrders);
+router.route("/order/:id").get(isAuthenticatedUser, getSingleOrder);
+router
+  .route("/admin/order")
+  .get(isAuthenticatedUser, authorizeRoles("admin"), allOrders);
+
+router
+  .route("/admin/order/:id")
+  .put(isAuthenticatedUser, authorizeRoles("admin"), updateOrder);
+
+router
+  .route("/admin/order/:id")
+  .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteOrder);
 
 module.exports = router;
