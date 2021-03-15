74f2913902bed8f63dbe44f5de65a1d248f94176
harshv9988 <harshv9988@gmail.com>
2021-03-15 17:37:41 +0530

    added react alert and single product page reducer actions

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -5,6 +5,9 @@ import {
   ALL_PRODUCTS_SUCCESS,
   ALL_PRODUCTS_FAIL,
   CLEAR_ERRORS,
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_SUCCESS,
+  PRODUCT_DETAILS_FAIL,
 } from "../constants/productConstants";
 
 export const getProducts = () => async (dispatch) => {
@@ -22,7 +25,27 @@ export const getProducts = () => async (dispatch) => {
   } catch (error) {
     dispatch({
       type: ALL_PRODUCTS_FAIL,
-      payload: error.response.data.message,
+      payload: error.response.data.errMessage,
+    });
+  }
+};
+
+export const getProductDetails = (id) => async (dispatch) => {
+  try {
+    dispatch({
+      type: PRODUCT_DETAILS_REQUEST,
+    });
+
+    const { data } = await axios.get(`/api/v1/products/${id}`);
+
+    dispatch({
+      type: PRODUCT_DETAILS_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: PRODUCT_DETAILS_FAIL,
+      payload: error.response.data.errMessage,
     });
   }
 };
--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -2,18 +2,24 @@ import React, { useEffect } from "react";
 import MetaData from "./layout/MetaData";
 
 import { useDispatch, useSelector } from "react-redux";
+import { useAlert } from "react-alert";
+
 import { getProducts } from "../actions/productActions";
-import Product from "../product/Product";
+import Product from "./product/Product";
 import Loader from "./layout/Loader";
 
 const Home = () => {
+  const alert = useAlert();
   const dispatch = useDispatch();
   const { loading, products, error, productsCount } = useSelector(
     (state) => state.products
   );
   useEffect(() => {
+    if (error) {
+      return alert.error(error);
+    }
     dispatch(getProducts());
-  }, [dispatch]);
+  }, [dispatch, alert, error]);
 
   return (
     <>
