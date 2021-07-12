a503e0259e31f0a1533813ead93a516303498922
harshv9988 <harshv9988@gmail.com>
2021-07-12 20:26:18 +0530

    font url changed

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -6,7 +6,6 @@ const User = require("../models/user");
 // Checks if user is authenticated or not
 exports.isAuthenticatedUser = catchAsyncErrors(async (req, res, next) => {
   const { token } = req.cookies;
-
   if (!token) {
     return next(new ErrorHandler("Login first to access this resource.", 401));
   }
